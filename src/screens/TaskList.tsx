771e17c68bc8f6665a759a63631355f9427b4e79
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-14 08:55:25 -0300

    Adicionando estilo ao TaskList

---
+++ src/screens/TaskList.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground style={styles.background} source={todayImage}>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>
+                        TaskList
+                    </Text>
+                </View>
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }
+})
