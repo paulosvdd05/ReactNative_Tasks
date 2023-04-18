57191f611c67b983c3042aebdedfb4b889bb58a2
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-18 08:54:37 -0300

    adicionando flatlist para mostrar tasks

--- src/screens/TaskList.tsx
+++ src/screens/TaskList.tsx
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 //moment
@@ -10,22 +10,79 @@ import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
+
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: 'Comprar Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }
+        ]
+    }
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground style={styles.background} source={todayImage}>
-                <View style={styles.titleBar}>
-                    <Text style={styles.tittle}>Hoje</Text>
-                    <Text style={styles.subtittle}>{today}</Text>
-                </View>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.tittle}>Hoje</Text>
+                        <Text style={styles.subtittle}>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Task desc="Comprar Livro" estimateAt={new Date()}
-                    doneAt={new Date()} />
-                    <Task desc="Ler Livro" estimateAt={new Date()}
-                    doneAt={null} />
-                    
+                    <FlatList
+                        data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} />}
+                    />
+
                 </View>
             </View>
 
@@ -47,18 +104,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-end'
     },
-    tittle:{
+    tittle: {
         fontFamily: commonStyles.fontFamily,
         fontSize: 50,
         color: commonStyles.colors.secundary,
-        marginLeft:20,
-        marginBottom:20
+        marginLeft: 20,
+        marginBottom: 20
     },
-    subtittle:{
+    subtittle: {
         fontFamily: commonStyles.fontFamily,
         fontSize: 20,
         color: commonStyles.colors.secundary,
-        marginLeft:20,
-        marginBottom:30
+        marginLeft: 20,
+        marginBottom: 30
     }
 })
