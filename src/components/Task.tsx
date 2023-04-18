67aff29dd13549f8f150ff697dfdf4d5193e487d
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-18 08:41:21 -0300

    Adicionando funcao getCheckView

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+
+import commonStyles from '../commonStyles'
+
 export default props => {
     return (
         <View style={styles.container}>
-            <Text>{props.desc}</Text>
-            <Text>{props.estimateAt + ""}</Text>
-            <Text>{props.doneAt + ""}</Text>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.desc}</Text>
+                <Text>{props.estimateAt + ""}</Text>
+            </View>
+
+
+
         </View>
 
     )
+}
+
+function getCheckView(doneAt) {
+    if(doneAt != null){
+        return (
+            <View><Text>Concluida</Text></View>
+        )
+    } else{
+        return (
+            <View><Text>Pendente</Text></View>
+        )
+    }
+    
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +42,8 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10
+    },
+    checkContainer: {
+        width: '20%'
     }
 })
