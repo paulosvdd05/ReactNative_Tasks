f4b7726dcdcecfe50aad7336ba434b392e753c3b
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-19 08:35:35 -0300

    atribuindo estilo ao modal

--- src/commonStyles.tsx
+++ src/commonStyles.tsx
@@ -1,6 +1,7 @@
 export default {
     fontFamily: 'Lato',
     colors: {
+        today: '#B13B44',
         secundary: '#FFF',
         mainText: '#222',
         subText: '#555'
--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
-
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import commonStyles from '../commonStyles'
 export default class AddTask extends Component {
     render() {
         return (
             <Modal transparent={true} visible={this.props.isVisible}
-                onRequestClose={this.props.onCancel} animationType='slide'>
+                onRequestClose={this.props.onCancel} animationType='fade'>
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
-                    <View style={styles.background}>
-
-                    </View>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
 
             </Modal>
@@ -21,5 +25,18 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    header:{
+        fontFamily:commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secundary,
+        textAlign:'center',
+        padding: 15,
+        fontSize: 18
+
     }
 })
