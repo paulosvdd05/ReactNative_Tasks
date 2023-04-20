253a4088f3986f08130a3ab8e651b4119e1a4a4c
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-20 08:48:20 -0300

    adicionando swipeable

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,54 +1,82 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
-import {Icon} from '@rneui/themed'
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import { Icon } from '@rneui/themed'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
 
 import commonStyles from '../commonStyles'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default props => {
 
     const doneOrNotStyle = props.doneAt != null ?
-        {textDecorationLine: 'line-through'} : {}
+        { textDecorationLine: 'line-through' } : {}
 
-        const date = props.doneAt ? props.doneAt : props.estimateAt
-        const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
+
+    const getRightContent = () => {
+        return (
+            <TouchableOpacity style={styles.right}>
+                <Icon name='trash' size={30} color='#FFF' type='font-awesome' />
+            </TouchableOpacity>
+        )
+    }
+    const getLeftContent = () => {
+        return (
+            <View style={styles.left}>
+                <Icon name='trash' size={20} color='#FFF' type='font-awesome' 
+                style={styles.excludeIcon} />
+                <Text style={styles.excludeText}>Excluir</Text>
+            </View>
+        )
+    }
 
     return (
-        <View style={styles.container}>
-            <TouchableWithoutFeedback
-            onPress={() => props.toggleTask(props.id)}>
-            <View style={styles.checkContainer}>
-                {getCheckView(props.doneAt)}
-            </View>
-            </TouchableWithoutFeedback>
-            <View>
-                <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-                <Text style={styles.date}>{formattedDate}</Text>
-            </View>
+        <GestureHandlerRootView>
+            <Swipeable
+                renderRightActions={getRightContent}
+                renderLeftActions={getLeftContent}>
+
+
+                <View style={styles.container}>
+                    <TouchableWithoutFeedback
+                        onPress={() => props.toggleTask(props.id)}>
+                        <View style={styles.checkContainer}>
+                            {getCheckView(props.doneAt)}
+                        </View>
+                    </TouchableWithoutFeedback>
+                    <View>
+                        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+                        <Text style={styles.date}>{formattedDate}</Text>
+                    </View>
 
 
 
-        </View>
+                </View>
+            </Swipeable>
+        </GestureHandlerRootView>
+
 
     )
 }
 
 function getCheckView(doneAt) {
-    if(doneAt != null){
+    if (doneAt != null) {
         return (
             <View style={styles.done}>
                 <Icon name='check' size={20} type='font-awesome' color='#FFF' />
             </View>
         )
-    } else{
+    } else {
         return (
             <View style={styles.pending}></View>
         )
     }
-    
+
 }
 
 const styles = StyleSheet.create({
@@ -57,36 +85,59 @@ const styles = StyleSheet.create({
         borderColor: '#AAA',
         borderBottomWidth: 1,
         alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 10,
+        backgroundColor: '#FFF'
     },
     checkContainer: {
         width: '20%',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    pending:{
-        height:25,
-        width:25,
-        borderRadius:13,
-        borderWidth:1,
+    pending: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
         borderColor: '#555'
     },
-    done:{
-        height:25,
-        width:25,
-        borderRadius:13,
+    done: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
         backgroundColor: '#4D7031',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    desc:{
+    desc: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.mainText,
-        fontSize:15
+        fontSize: 15
     },
     date: {
         fontFamily: commonStyles.fontFamily,
-        color:commonStyles.colors.subText,
+        color: commonStyles.colors.subText,
         fontSize: 12
+    },
+    right: {
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20
+    },
+    left: {
+        flex:1,
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    excludeText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        margin: 10
+    },
+    excludeIcon:{
+        marginLeft: 10
     }
 })
