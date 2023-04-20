18891d1f5739dc230bb42588ccda39db79cd667a
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-20 08:29:33 -0300

    arrumando datePicker do android

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput, Platform } from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import commonStyles from '../commonStyles'
+import moment from 'moment'
 
-const initialState ={ desc: '', date: new Date()}
+const initialState = { desc: '', date: new Date(), showDatePicker: false }
 export default class AddTask extends Component {
 
     state = {
         ...initialState
     }
 
-    getDateTimePicker = () =>{
-        return <DateTimePicker 
+    getDatePicker = () => {
+        let datePicker = <DateTimePicker
             value={this.state.date}
-            onChange={(_, date) => this.setState({date})}
-            mode='date'/>
+            onChange={(_, date) => this.setState({ date, showDatePicker: false })}
+            mode='date' />
+
+        const dateString = moment(this.state.date).format('ddd, D [de] MMMM [de] YYYY')
+
+        if (Platform.OS === 'android') {
+            datePicker = (
+                <View>
+                    <TouchableOpacity onPress={() => this.setState({showDatePicker: true})}>
+                        <Text style={styles.date}>
+                            {dateString}
+                        </Text>
+                    </TouchableOpacity>
+                    {this.state.showDatePicker && datePicker}
+                </View>
+            )
+        }
+
+        return datePicker
     }
 
     render() {
@@ -29,14 +47,14 @@ export default class AddTask extends Component {
                     <TextInput style={styles.input}
                         placeholder='Informe a Descrição...'
                         value={this.state.desc}
-                        onChangeText={desc => this.setState({desc})}
+                        onChangeText={desc => this.setState({ desc })}
                     />
-                    {this.getDateTimePicker()}
+                    {this.getDatePicker()}
                     <View style={styles.buttons}>
                         <TouchableOpacity onPress={this.props.onCancel}>
                             <Text style={styles.button}>Cancelar</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity>  
+                        <TouchableOpacity>
                             <Text style={styles.button}>Salvar</Text>
                         </TouchableOpacity>
                     </View>
@@ -58,30 +76,35 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff'
     },
-    header:{
-        fontFamily:commonStyles.fontFamily,
+    header: {
+        fontFamily: commonStyles.fontFamily,
         backgroundColor: commonStyles.colors.today,
         color: commonStyles.colors.secundary,
-        textAlign:'center',
+        textAlign: 'center',
         padding: 15,
         fontSize: 18
     },
-    buttons:{
-        flexDirection:'row',
-        justifyContent:'flex-end',
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
 
     },
-    button:{
-        margin:20,
-        marginRight:30,
-        color:commonStyles.colors.today
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     },
-    input:{
-        fontFamily:commonStyles.fontFamily,
-        height:40,
-        margin:15,
-        backgroundColor:'#fff',
-        borderWidth:1,
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#fff',
+        borderWidth: 1,
         borderColor: '#E4E4E3'
+    },
+    date: {
+        fontFamily : commonStyles.fontFamily,
+        fontSize: 20,
+        marginLeft: 15
     }
 })
