f0729fbb2114627063965592088863a44c031bfa
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-05-03 09:01:55 -0300

    adicionando condicao aos componentes do formulario

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -8,16 +8,30 @@ import commonStyles from '../commonStyles'
 export default class Auth extends Component {
 
     state = {
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: '',
+        stageNew: true
+
     }
 
     render() {
         return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
-                <Text style={styles.tittle}>Tasks</Text>
+                <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formContainer}>
+                    <Text style={styles.subTitle}>
+                        {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+                    </Text>
+                    {this.state.stageNew &&
+                        <TextInput placeholder='Nome'
+                            value={this.state.name}
+                            style={styles.input}
+                            onChangeText={name => this.setState({ name })} />
+                    }
+
                     <TextInput placeholder='E-mail'
                         value={this.state.email}
                         style={styles.input}
@@ -25,10 +39,19 @@ export default class Auth extends Component {
                     <TextInput placeholder='Senha'
                         value={this.state.password}
                         style={styles.input}
+                        secureTextEntry={true}
                         onChangeText={password => this.setState({ password })} />
+                    {this.state.stageNew &&
+                        <TextInput placeholder='Confirmação de Senha'
+                            value={this.state.confirmPassword}
+                            style={styles.input}
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })} />
+                    }
                     <TouchableOpacity>
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -45,11 +68,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    tittle: {
+    title: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secundary,
         fontSize: 70,
         marginBottom: 10
+    },
+    subTitle:{
+        fontFamily: commonStyles.fontFamily,
+        color: '#fff',
+        fontSize: 20,
+        textAlign:'center',
+        marginBottom:10
     },
     input: {
         marginTop: 10,
@@ -61,15 +91,15 @@ const styles = StyleSheet.create({
         padding: 20,
         width: '90%'
     },
-    button:{
-        backgroundColor: '#080',
+    button: {
+        backgroundColor: '#070',
         marginTop: 10,
-        padding:10,
-        alignItems:'center'
+        padding: 10,
+        alignItems: 'center'
     },
-    buttonText:{
-        fontFamily:commonStyles.fontFamily,
-        color:'#fff',
-        fontSize:20
+    buttonText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#fff',
+        fontSize: 20
     }
 })
