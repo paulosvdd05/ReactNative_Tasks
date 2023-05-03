23938472241bf00150b17d9a8fe02dc97681b159
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-05-03 08:48:05 -0300

    adicionando formulario

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,15 +1,37 @@
-import React, {Component} from 'react'
-import {View, Text, ImageBackground, StyleSheet} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, ImageBackground, StyleSheet, TextInput, TouchableOpacity, Platform } from 'react-native'
+
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 
 export default class Auth extends Component {
-    render(){
-        return(
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    render() {
+        return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
                 <Text style={styles.tittle}>Tasks</Text>
+                <View style={styles.formContainer}>
+                    <TextInput placeholder='E-mail'
+                        value={this.state.email}
+                        style={styles.input}
+                        onChangeText={email => this.setState({ email })} />
+                    <TextInput placeholder='Senha'
+                        value={this.state.password}
+                        style={styles.input}
+                        onChangeText={password => this.setState({ password })} />
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
 
         )
@@ -17,16 +39,37 @@ export default class Auth extends Component {
 }
 
 const styles = StyleSheet.create({
-    background:{
-        flex:1,
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'center'
+    background: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    tittle:{
+    tittle: {
         fontFamily: commonStyles.fontFamily,
-        color:commonStyles.colors.secundary,
+        color: commonStyles.colors.secundary,
         fontSize: 70,
-        marginBottom:10
+        marginBottom: 10
     },
+    input: {
+        marginTop: 10,
+        backgroundColor: '#fff',
+        padding: Platform.OS == 'ios' ? 15 : 10
+    },
+    formContainer: {
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        padding: 20,
+        width: '90%'
+    },
+    button:{
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding:10,
+        alignItems:'center'
+    },
+    buttonText:{
+        fontFamily:commonStyles.fontFamily,
+        color:'#fff',
+        fontSize:20
+    }
 })
