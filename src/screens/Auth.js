28fac62916ecaea92da13135173c7de5af01e963
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-05-10 09:37:16 -0300

    arrumando menu.tsx

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -57,7 +57,7 @@ export default class Auth extends Component {
             })
 
             axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-            this.props.navigation.navigate('Home')
+            this.props.navigation.navigate('Home', res.data)
         }catch(e){
             showError(e)
         }
