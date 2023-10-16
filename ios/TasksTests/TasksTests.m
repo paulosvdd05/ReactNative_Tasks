9555acbf61e6eb896f0d31593bc4d20f4b018730
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-10-16 20:51:09 -0300

    trocando nome pacote

--- ios/TasksTests/TasksTests.m
+++ ios/TasksTests/TasksTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TasksTests : XCTestCase
+@interface tasksTests : XCTestCase
 
 @end
 
-@implementation TasksTests
+@implementation tasksTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
